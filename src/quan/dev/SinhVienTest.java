0f3042a87d3be3a23810fd51a3440d3bc83ad73a
TranHongQuan <quantran.130798@gmail.com>
2018-08-22 15:14:44 +0700

    Class Sinh Vien

---
+++ src/quan/dev/SinhVienTest.java
@@ -0,0 +1,12 @@
+package quan.dev;
+
+public class SinhVienTest {
+	public static void main(String[] args) {
+		SinhVien sv = new SinhVien();
+		sv.hoTen = "Tran Hong Quan";
+		sv.maSV = 1234;
+		sv.thi();
+		sv.dongHocPhi();
+	}
+
+}
